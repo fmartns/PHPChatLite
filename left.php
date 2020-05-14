d3e0bdd5e1d466bcfc2044d012b0af4690003dea
Filipe <fmartns6@Gmail.com>
2020-05-14 17:12:51 -0300

    Update left.php

--- left.php
+++ left.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="refresh" content="0; url=index.htm">
-        <title>Chat | Left Code</title>
-    </head>
-    <body>
-    <?php
-    $nick = $_POST['nick']; 
-    $left = fopen("chat.txt", "a");
-    if($left) {
-        fwrite($left,"<b>{$nick}<i> left the chat.</i></b><br>");
-    }
-        fclose($left);
-    ?>
-    </body>
-</html>
+<?php
+$nick = $_POST['nick']; 
+$left = fopen("chat.txt", "a");
+if($left) {
+    fwrite($left,"<b>{$nick}<i> left the chat.</i></b><br>");
+}
+    fclose($left);
+?>
